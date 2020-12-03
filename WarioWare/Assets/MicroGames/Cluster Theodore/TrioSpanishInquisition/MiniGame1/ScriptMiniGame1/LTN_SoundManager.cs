f1715b15083dd40dd079965a339b3cc24e646d56
Luana-GD <lmtn2706@gmail.com>
2020-12-03 15:53:14 +0100

    audio manager créé

---
+++ WarioWare/Assets/MicroGames/Cluster Theodore/TrioSpanishInquisition/MiniGame1/ScriptMiniGame1/LTN_SoundManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpanishInquisition
+{
+    namespace MiniGame1
+    {
+        public class LTN_SoundManager : MonoBehaviour
+        {
+            private AudioSource[] gameSounds;
+
+            private AudioSource goodButton;
+            private AudioSource wrongButton;
+
+            // Start is called before the first frame update
+            void Start()
+            {
+                gameSounds = GetComponents<AudioSource>();
+
+                goodButton = gameSounds[0];
+                wrongButton = gameSounds[1];
+            }
+
+            public void PlayGoodButton()
+            {
+                goodButton.Play();
+                Debug.Log("Goodbutton play.");
+            }
+
+            public void PlayWrongButton()
+            {
+                wrongButton.Play();
+                Debug.Log("WronButton play");
+            }
+        }
+    }
+}
