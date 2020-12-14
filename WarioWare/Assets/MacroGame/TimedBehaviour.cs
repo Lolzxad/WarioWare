3268e9f27ac3cb4542f3e9cf6d33f485977ab2b3
Adel <adelou001@hotmail.com>
2020-12-14 18:57:50 +0100

    Fix

--- WarioWare/Assets/MacroGame/TimedBehaviour.cs
+++ WarioWare/Assets/MacroGame/TimedBehaviour.cs
@@ -50,12 +50,6 @@ public class TimedBehaviour : MonoBehaviour
     {
         
     }
-            // Start is called before the first frame update
-            void Start()
-            {
-
-            }
-
             // Update is called once per frame
             private void Update()
             {
